c9703c8ece2f6a2ed80f2022fe290fedd4b04e68
Aarush Acharya <92685647+Aarush-Acharya@users.noreply.github.com>
2024-11-05 17:04:55 +0530

    feat: started writing controller for bussiness logic of chats in rooms and upcoming rooms

---
+++ lib/controllers/room_chat_controller.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+import 'package:resonate/models/message.dart';
+
+class RoomChatController extends GetxController {
+  RxList<Message> roomMessages = <Message>[].obs;
+
+  Future<void> fetchRoomMessages() async {
+    
+
+  }
+
+}
